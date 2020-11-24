35c3b61b872eb42492bb944db20fb3485a0e2d4a
Dario Castañé <d@rio.hn>
2020-11-24 23:00:58 +0100

    Fix configuration for new languages

--- config.rb
+++ config.rb
@@ -27,7 +27,7 @@ page '/*.txt', layout: false
 # Proxy pages
 # https://middlemanapp.com/advanced/dynamic-pages/
 
-available_locales = %i[bg ca cs da de el es en eo et eu fi fr ga hr hu it lb lt lv mt nl oc pl pt ro ru sk sl sv]
+available_locales = %w[ar bg ca cs da de el es en eo et eu fi fr ga hr hu it lb lt lv mt nl oc pl pt ro ru sk sl sv tr]
 
 available_locales.each do |locale|
   next if locale == :en
